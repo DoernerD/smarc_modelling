dde6f7228d5e86e4b630b6a46913e0f617fb46b6
David <ddorner@kth.se>
2026-01-16 14:43:56 +0100

    Sim MPC tuning trajectory tracking

--- src/smarc_modelling/control/control.py
+++ src/smarc_modelling/control/control.py
@@ -39,18 +39,18 @@ class NMPC:
         Q_diag[14] = 1e-5            # LCG:      Standard: 1e-4
         Q_diag[15] = 5e2             # stern_angle:   Standard: 100
         Q_diag[16] = 1e2             # rudder_angle:  Standard: 100
-        Q_diag[17:] = 1e-3            # RPM1 And RPM2: Standard: 1e-6
+        Q_diag[17:] = 1e-6 #1e-3            # RPM1 And RPM2: Standard: 1e-6
         #Q_diag[13:] = Q_diag[13:  ]   # Adjustment to all control weights
         Q = np.diag(Q_diag)
 
         # Control rate of change weight matrix - control inputs as [x_vbs, x_lcg, delta_s, delta_r, rpm1, rpm2]
         R_diag = np.ones(self.nu)
-        R_diag[0] = 1e-1        # VBS
+        R_diag[0] = 1e-2 #1e-1        # VBS
         R_diag[1] = 1e-1        # LCG
         R_diag[2] = 1e2
         R_diag[3] = 1e3
         R_diag[4: ] = 1e-5
-        R = np.diag(R_diag)*1e-3
+        R = np.diag(R_diag)#*1e-3
 
         # Stage costs
         self.model.p = ca.MX.sym('ref_param', self.nx+self.nu,1)
