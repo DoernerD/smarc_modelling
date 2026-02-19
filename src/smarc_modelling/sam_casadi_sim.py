Fixing quaternion order in plotting

src/smarc_modelling/sam_casadi_sim.py
@@ -8,7 +8,7 @@ from smarc_modelling.lib import *
 from smarc_modelling.vehicles import *
 from smarc_modelling.vehicles.SAM_casadi import SAM_casadi
 
-matplotlib.use("TkAgg")  # or 'Qt5Agg', depending on what you have installed
+matplotlib.use("Qt5Agg")  # or 'Qt5Agg', depending on what you have installed
 
 # Initial conditions
 eta0 = np.zeros(7)
@@ -61,11 +61,11 @@ def run_simulation(t_span, x0, dt, sam):
     """
 
     u = np.zeros(6)
-    u[0] = 100  # *np.sin((i/(20/0.02))*(3*np.pi/4))        # VBS
+    u[0] = 50  # *np.sin((i/(20/0.02))*(3*np.pi/4))        # VBS
     u[1] = 50  # LCG
     u[2] = 0  # np.deg2rad(7)    # Vertical (stern)
-    u[3] = 0  # -np.deg2rad(7)  # Horizontal (rudder)
-    u[4] = 0  # 1000     # RPM 1
+    u[3] = -np.deg2rad(7)  # Horizontal (rudder)
+    u[4] = 1000     # RPM 1
     u[5] = u[4]  # RPM 2
 
     # Run integration
@@ -93,18 +93,22 @@ def plot_results(sol):
     """
 
     def quaternion_to_euler_vec(sol):
-
+        # State quaternion is [q0, q1, q2, q3] = (w, x, y, z). Scipy from_quat
+        # expects (x, y, z, w), so pass [q1, q2, q3, q0].
+        # as_euler("xyz") returns (roll, pitch, yaw) = (phi, theta, psi).
         n = len(sol.y[3])
         psi = np.zeros(n)
         theta = np.zeros(n)
         phi = np.zeros(n)
 
         for i in range(n):
-            q = [sol.y[3, i], sol.y[4, i], sol.y[5, i], sol.y[6, i]]
-            rot = R.from_quat(q)
-            rot_euler = rot.as_euler("xyz")
-            psi[i], theta[i], phi[i] = rot_euler
-            # psi[i], theta[i], phi[i] = gnc.quaternion_to_angles(q)
+            q_w, q_x, q_y, q_z = sol.y[3, i], sol.y[4, i], sol.y[5, i], sol.y[6, i]
+            q_xyzw = [q_x, q_y, q_z, q_w]
+            rot = R.from_quat(q_xyzw)
+            roll, pitch, yaw = rot.as_euler("xyz")
+            phi[i] = roll
+            theta[i] = pitch
+            psi[i] = yaw
 
         return psi, theta, phi
 